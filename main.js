8ac6422337aa43ddb34232720842d1733d376237
Marianela Alvarez <alvarez_mm@hotmail.com>
2022-10-17 20:37:00 -0300

    inicio

---
+++ main.js
@@ -0,0 +1,48 @@
+class Usuario {
+	constructor (nombre, apellido, libros, mascotas) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.libros = libros;
+		this.mascotas = mascotas;
+	}
+           
+    getFullName () {
+        return `${this.nombre} ${this.apellido}`;
+    }
+
+    addMascota(nuevaMascota) {
+        return this.mascotas.push(nuevaMascota);
+    }
+
+    countMascotas() {
+        return this.mascotas.length;
+    }
+
+    addBook(titulo, autor) {
+   		return this.libros.push({titulo, autor});
+    }
+
+	getBookNames() {
+		const newArray = this.libros.filter(item => item.libros === "titulo");
+    }
+}
+
+const usuario1 = new Usuario ("María", "Alvarez", [{titulo: "Harry Potter", autor: "J K Rowling"} , {titulo: "El Proceso", autor: "Kafka"}] ,  ["perro", "gato"]);
+
+console.log (usuario1.getFullName());
+
+usuario1.getFullName();
+
+usuario1.addMascota("pez");
+
+console.log(usuario1);
+
+console.log (usuario1.countMascotas());
+
+usuario1.countMascotas();
+
+usuario1.addBook("El libro de la Selva", "Rudyard Kipling");
+
+console.log (usuario1);
+
+console.log (usuario1.getBookNames());
